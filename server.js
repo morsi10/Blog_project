86b784ea8b8b74473a2c28c2d3f36b1cd4f5e5a8
Morsi Hamza <morsi4394@gmail.com>
2022-08-22 11:04:18 +0100

    heroku-config

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/mini-projet1'));
 app.get('/*', function(req,res)
 
-{res.sendFile(path.join(__dirname+'/dist/mini-projet1'));});
+{res.sendFile(path.join(__dirname+'/dist/mini-projet1/index.html'));});
 
 app.listen(process.env.PORT || 8080);
